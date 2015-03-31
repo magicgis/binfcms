19e41602c1730913012dfbbb8744c0f6a4ffcf8a
burgleaf <burgleaf@gmail.com>
2015-03-31 23:51:25 +0800

    添加shiro

---
+++ binf-admin/src/main/java/me/binf/admin/auth/BinRealm.java
@@ -0,0 +1,30 @@
+package me.binf.admin.auth;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Created by wangbin on 2015/3/31.
+ */
+public class BinRealm extends AuthorizingRealm implements Realm, InitializingBean {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
